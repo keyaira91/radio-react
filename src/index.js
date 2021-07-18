70ff4e5c4c919c20595ade9e34e287ca088a9c5d
keyaira91 <keyaira22@hotmail.com>
2021-07-17 23:57:57 -0400

    added radio butttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class Radio extends React.Component {
+  chooseColor(e) {
+    console.log(e.target.value);
+  }
+
+  render() {
+    return (
+      <div className="container mt-5">
+      <div className="card" onChange={this.chooseColor.bind(this)}>
+        <form className="card-body">
+        <h2>Select Color:</h2>
+          <input type="radio" value="red" name="color"/>Red<br/>
+          <input type="radio" value="green" name="color"/>Green<br/>
+          <input type="radio" value="blue" name="color"/>Blue<br/>
+          <input type="radio" value="orange" name="color"/>Orange<br/>
+          <input type="radio" value="purple" name="color"/>Purple<br/>
+          <h3></h3>
+          <button className="btn btn-sm btn-primary mt-3 mb-3">Choose Color</button>
+          <p></p>
+        </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+ReactDOM.render(<Radio/>, document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

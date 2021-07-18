3b092d4e108c5a4f6aab64e773e83ebc59c92dd5
keyaira91 <keayira22@hotmail.com>
2021-07-18 16:56:44 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Radio extends React.Component {
-  chooseColor(e) {
-    console.log(e.target.value);
-  }
-
-  render() {
-    return (
-      <div className="container mt-5">
-      <div className="card">
-        <form className="card-body" onChange={this.chooseColor.bind(this)}>
-        <h2>Select Color:</h2>
-          <input type="radio" value="red" name="color"/>Red<br/>
-          <input type="radio" value="green" name="color"/>Green<br/>
-          <input type="radio" value="blue" name="color"/>Blue<br/>
-          <input type="radio" value="orange" name="color"/>Orange<br/>
-          <input type="radio" value="purple" name="color"/>Purple<br/>
-          <h3></h3>
-          <button className="btn btn-sm btn-outline-secondary mt-3 mb-3" type="sumbit">Choose Color</button>
-          <p></p>
-        </form>
-        </div>
-      </div>
-    )
-  }
-}
+import Radio from './radio';
 
 
 
-
-
-
-
-
-ReactDOM.render(<Radio/>, document.getElementById('root')
+ReactDOM.render(
+<Radio/>, document.getElementById('root')
 );

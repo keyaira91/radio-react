5dd1811a1cea43abf3fd9a1c07d0997906f20ce3
keyaira91 <keayira22@hotmail.com>
2021-07-18 21:49:04 -0400

    sumbit btn working with color choice

--- src/radio.js
+++ src/radio.js
@@ -1,57 +1,71 @@
 import React from 'react';
 
-
-
- function Radio(){
-    const afterClick = () => {
-        alert("Hi")
+class Radio extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            color: ""
+        };
+        this.onColorChoice = this.onColorChoice.bind(this);
+        this.colorSumbit = this.colorSumbit.bind(this);
     }
 
 
-    return (
+
+  onColorChoice(event) {
+      this.setState({
+         selectedColor: event.target.value});
+      
+  }
+
+  colorSumbit(event) {
+   event.preventDefault();
+   console.log(this.state.selectedColor)
+    
+}
+
+    
+    render() {
+        return (
         <div className="container mt-5">
             <div className="card mx-auto" style={{width: "25rem"}}>
                 <div className="card-body">
                     <h2>Select Color:</h2>
-                        <form>
-                            <input type="radio" value="red" name="color"/>Red<br/>
-                            <input type="radio" value="green" name="color"/>Green<br/>
-                            <input type="radio" value="blue" name="color"/>Blue<br/>
-                            <input type="radio" value="orange" name="color"/>Orange<br/>
-                            <input type="radio" value="purple" name="color"/>Purple<br/>
-                            <button onClick={afterClick} className="btn btn-sm btn-outline-secondary mt-3 mb-3" type="sumbit">Choose Color</button>
+                        <form onSubmit={this.colorSumbit}>
+                            <input type="radio" value="red" name="color"
+                            checked={this.state.selectedColor === "red"}
+                            onChange={this.onColorChoice}/>
+                            <label>Red</label><br/>
+
+                            <input type="radio" value="green" name="color"
+                            checked={this.state.selectedColor === "green"}
+                            onChange={this.onColorChoice}/>
+                            <label>Green</label><br/>
+
+                            <input type="radio" value="blue" name="color"
+                            checked={this.state.selectedColor === "blue"}
+                            onChange={this.onColorChoice}/>
+                            <label>Blue</label><br/>
+
+                            <input type="radio" value="orange" name="color"
+                            checked={this.state.selectedColor === "orange"}
+                            onChange={this.onColorChoice}/>
+                            <label>Orange</label><br/>
+
+                            <input type="radio" value="purple" name="color"
+                            checked={this.state.selectedColor === "purple"}
+                            onChange={this.onColorChoice}/>
+                            <label>Purple</label><br/>
+                            
+                            <button onClick={this.colorSumbit} className="btn btn-sm btn-outline-secondary mt-3 mb-3">Choose Color</button>
                         </form>
                 </div>
             </div>
         </div>
      )
     
+    }
 }
 
 
-
 export default Radio;
-    
-//chooseColor(e) {
-//       console.log(e.target.value);
-//     }
-  
-//     render() {
-//       return (
-//         <div className="container mt-5">
-//         <div className="card">
-//           <form>
-//          
-//             <input type="radio" value="red" name="color"/>Red<br/>
-//             <input type="radio" value="green" name="color"/>Green<br/>
-//             <input type="radio" value="blue" name="color"/>Blue<br/>
-//             <input type="radio" value="orange" name="color"/>Orange<br/>
-//             <input type="radio" value="purple" name="color"/>Purple<br/>
-//             <h3></h3>
-//             <button className="btn btn-sm btn-outline-secondary mt-3 mb-3" type="sumbit">Choose Color</button>
-//             <p></p>
-//           </form>
-//           </div>
-//         </div>
-//       )
-//     }
